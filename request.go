4580bcd5bbf71c8a7c47c76deaf1a5fae1d3d592
Jackson Sabey <jackson@juandefu.ca>
2016-07-31 02:38:58 -0700

    renamed namespace moved namespace above copyright

--- request.go
+++ request.go
@@ -1,6 +1,7 @@
+package transmission
+
 // Copyright 2015, JuanDeFu.ca. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
-package transmission
 
 /*
 Requests support three keys:
--- transmission_test.go
+++ transmission_test.go
@@ -1,12 +1,13 @@
+package transmission
+
 // Copyright 2015, JuanDeFu.ca. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
-package transmission
 
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/sabey/unittest"
 	"log"
+	"sabey.co/unittest"
 	"testing"
 )
 
